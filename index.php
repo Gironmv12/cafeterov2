3399bf2bffdccdba6c44f9525c790f689c4cf770
Gama2176 <gamalielmtz76@gmail.com>
2023-09-28 18:08:42 -0600

    Agregre el h1 y el lorem

--- index.php
+++ index.php
@@ -6,10 +6,9 @@
     <title>Cafétero</title>
 </head>
 <body>
-    <h1>Indedx de la pagina</h1>
-    <p>Ejemplo de la rama backend</p>
 
-    <p> Jaja el giron si</p>
+    <h1> Cafeteria Cafe-tero </h1>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore debitis aliquam quod doloribus culpa dolores ducimus ullam molestias possimus tempora dolor, voluptas neque ratione, voluptatem tempore totam necessitatibus, perspiciatis a? </p>
     
 </body>
 </html>
