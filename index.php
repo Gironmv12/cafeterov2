978a1c6e0e011e0dd8fd155835d2687e9bbfe6e7
Gama2176 <gamalielmtz76@gmail.com>
2023-09-29 01:11:01 -0600

    Agregué carpetas

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Cafétero</title>
 </head>
+
 <body>
 
-    <h1> Cafeteria Cafe-tero </h1>
-    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore debitis aliquam quod doloribus culpa dolores ducimus ullam molestias possimus tempora dolor, voluptas neque ratione, voluptatem tempore totam necessitatibus, perspiciatis a? </p>
-    
-    
+    <h1> Si </h1>
+
 </body>
 </html>
