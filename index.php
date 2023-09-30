8af6b5e428aa4bc32fc625e3439fb4f9c0ca57e6
Gama2176 <gamalielmtz76@gmail.com>
2023-09-30 03:32:54 -0600

    Creación de index

--- index.php
+++ index.php
@@ -5,12 +5,94 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Cafétero</title>
+    <!-- Link de heroicons -->
+    <link href="https://cdn.jsdelivr.net/npm/heroicons@1.0.1/dist/heroicons.min.css" rel="stylesheet">
+
+    <!-- icono de la página -->
+    <link rel="icon" href="./View/images/icono.png" type="png">
+
+    <!-- Enlace a estilos index.css -->
+    <link rel="stylesheet" href="View/Css/index.css">
+
+    <!-- Título de la página -->
+    <title>Café-tero</title>
 </head>
 
 <body>
 
-    <h1> Si </h1>
+    <!-- Navbar -->
+    <nav>
+        <div class="brand">
+            <h1><a href="#">Café-tero</a></h1>
+        </div>
+
+        <ul class="nav-list">
+            <li><a href="#">Menú</a></li>
+            <li><a href="#">Catálogo</a></li>
+            <li><a href="#">Reseñas</a></li>
+            <li><a href="#">Contacto</a></li>
+        </ul>
+    </nav>
+
+    <!-- Línea abajo del navbar -->
+    <hr class="custom-hr">
+
+    <!-- Título en el centro -->
+    <div class="container">
+        <h1> Café-tero </h1>
+    </div>
+
+    <!-- Salto de línea -->
+    <br> <br>
+
+    <!-- Icono1 parte inferior -->
+    <div class="icon1">
+        <a href="#">
+            <svg width="35" height="35" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M26.25 10C26.25 13.4518 23.4518 16.25 20 16.25C16.5482 16.25 13.75 13.4518 13.75 10C13.75 6.54822 16.5482 3.75 20 3.75C23.4518 3.75 26.25 6.54822 26.25 10Z"
+                    stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                <path
+                    d="M7.50189 33.5304C7.61907 26.7282 13.1698 21.25 20 21.25C26.8304 21.25 32.3812 26.7284 32.4981 33.5309C28.6935 35.2767 24.4607 36.25 20.0005 36.25C15.54 36.25 11.3068 35.2765 7.50189 33.5304Z"
+                    stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+        </a>
+    </div>
+
+    <!-- Icono 2 parte inferior -->
+    <div class="icon2">
+        <a href="#">
+            <svg width="30" height="30" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M15 21.25L18.75 25L25 16.25M35 20C35 28.2843 28.2843 35 20 35C11.7157 35 5 28.2843 5 20C5 11.7157 11.7157 5 20 5C28.2843 5 35 11.7157 35 20Z"
+                    stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+        </a>
+    </div>
+
+    <!-- Icono 3 parte inferior -->
+    <div class="icon3">
+        <a href="#">
+            <svg width="30" height="30" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M35 13.75C35 9.60786 31.5022 6.25 27.1875 6.25C23.9615 6.25 21.1921 8.12713 20 10.8057C18.8079 8.12713 16.0385 6.25 12.8125 6.25C8.49778 6.25 5 9.60786 5 13.75C5 25.7843 20 33.75 20 33.75C20 33.75 35 25.7843 35 13.75Z"
+                    stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+        </a>
+    </div>
+
+    <!-- Línea lateral inferior derecha -->
+    <hr class="custom-hr2">
+
+    <!-- Texto lateral inferior derecha -->
+    <p class="text">
+        Somos una empresa <br>dedicada a la venta de <br> café orgánico y artesanal.
+    </p>
+
+    <!-- imagen inferior izquierda -->
+    <img src="View/images/logo-1.png" alt="Logo">
+
 
 </body>
+
 </html>
