00a645b73f6e3a53f3984326065822feb8539ec2
Gama2176 <gamalielmtz76@gmail.com>
2023-10-07 00:01:43 -0600

    Creación de btn-busqueda y animación

--- View/Pages/menu.php
+++ View/Pages/menu.php
@@ -110,10 +110,21 @@ session_start();
                 <div>
                     <h2 id="saludo"></h2>
                 </div>
-                <div>
-                    <label for="second-search-input">Search:</label>
-                    <input type="search" id="second-search-input">
+
+                <!-- Búsqueda -->
+                <div class="btn-busqueda">
+                    <div class="busqueda">
+                        <svg class="icono-busqueda" aria-hidden="true" viewBox="0 0 24 24">
+                            <g>
+                                <path
+                                    d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z">
+                                </path>
+                            </g>
+                        </svg>
+                        <input placeholder="Buscar en el menú" type="search" class="input">
+                    </div>
                 </div>
+
                 <div>
                     <a href="" class="btn_login">Login</a>
                 </div>
@@ -164,10 +175,10 @@ session_start();
         </div>
         <div class="carrito">
 
-        <div class="descripcion">
-            <p class="orden">Orden actual</p>
-            <p id="fecha">Fecha:</p>
-        </div>
+            <div class="descripcion">
+                <p class="orden">Orden actual</p>
+                <p id="fecha">Fecha:</p>
+            </div>
 
         </div>
 
