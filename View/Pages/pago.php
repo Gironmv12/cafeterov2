2a5469e387fc362a73e38ffe3ecf6794e4476530
Gama2176 <gamalielmtz76@gmail.com>
2023-10-23 18:02:50 -0600

    Estilos

--- View/Pages/pago.php
+++ View/Pages/pago.php
@@ -17,43 +17,54 @@
 
 <body>
 
-    <!-- Maquetado -->
+    <div class="img-fondo">
 
-    <div class="parent">
+        <div class="contenido">
 
-        <div class="div1">
-            <p>Café-tero</p>
-            <div class="contenido">
+            <!-- Maquetado -->
 
-                <p>Transferencia Bancaria Directa.</p>
+            <div class="parent">
 
-                <div class="frame">
-                        <p>
-                            Banco: BBVA Bancomer, S.A. <br>
-                            Titular de la cuenta: Cafe-tero, S.A. de C.V. <br>
-                            Número de cuenta: 0147481705 <br>
-                            CLABE: 012100001474817059 <br>
-                        </p>
+                <div class="div1">
+                    <p>Café-tero</p>
+                    <div class="contenido">
+
+                        <p>Transferencia Bancaria Directa.</p>
+
+                        <div class="frame">
+                            <p>
+                                Banco: BBVA Bancomer, S.A. <br>
+                                Titular de la cuenta: Cafe-tero, S.A. de C.V. <br>
+                                Número de cuenta: 0147481705 <br>
+                                CLABE: 012100001474817059 <br>
+                            </p>
+                        </div>
+                        <p id="texto-t">Tu compra estará segura, al finalizar el pago enviaremos un comprobante de
+                            compra a tu
+                            correo
+                            electrónico.</p>
+
+                        <button class="boton-minimalista" href="#"> finalizar pago
+                            <svg width="18" height="18" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                    d="M11 2.0625C6.06396 2.0625 2.0625 6.06396 2.0625 11C2.0625 15.936 6.06396 19.9375 11 19.9375C15.936 19.9375 19.9375 15.936 19.9375 11C19.9375 6.06396 15.936 2.0625 11 2.0625ZM14.9236 11.4861C15.0526 11.3572 15.125 11.1823 15.125 11C15.125 10.8177 15.0526 10.6428 14.9236 10.5139L12.1736 7.76386C11.9052 7.49538 11.4699 7.49538 11.2014 7.76386C10.9329 8.03235 10.9329 8.46765 11.2014 8.73614L12.7777 10.3125L7.5625 10.3125C7.1828 10.3125 6.875 10.6203 6.875 11C6.875 11.3797 7.1828 11.6875 7.5625 11.6875L12.7777 11.6875L11.2014 13.2639C10.9329 13.5323 10.9329 13.9676 11.2014 14.2361C11.4698 14.5046 11.9051 14.5046 12.1736 14.2361L14.9236 11.4861Z"
+                                    fill="white" />
+                            </svg>
+                        </button>
+
+                        <button class="boton-regresar" href="../Pages/menu.php"> Regresar </button>
+                    </div>
                 </div>
-                <p id="texto-t">Tu compra estará segura, al finalizar el pago enviaremos un comprobante de compra a tu correo
-                    electrónico.</p>
 
-                    <button class="boton-minimalista" href="#" > finalizar pago 
-                        <svg width="18" height="18" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" clip-rule="evenodd"
-                                d="M11 2.0625C6.06396 2.0625 2.0625 6.06396 2.0625 11C2.0625 15.936 6.06396 19.9375 11 19.9375C15.936 19.9375 19.9375 15.936 19.9375 11C19.9375 6.06396 15.936 2.0625 11 2.0625ZM14.9236 11.4861C15.0526 11.3572 15.125 11.1823 15.125 11C15.125 10.8177 15.0526 10.6428 14.9236 10.5139L12.1736 7.76386C11.9052 7.49538 11.4699 7.49538 11.2014 7.76386C10.9329 8.03235 10.9329 8.46765 11.2014 8.73614L12.7777 10.3125L7.5625 10.3125C7.1828 10.3125 6.875 10.6203 6.875 11C6.875 11.3797 7.1828 11.6875 7.5625 11.6875L12.7777 11.6875L11.2014 13.2639C10.9329 13.5323 10.9329 13.9676 11.2014 14.2361C11.4698 14.5046 11.9051 14.5046 12.1736 14.2361L14.9236 11.4861Z"
-                                fill="white" />
-                        </svg>
-                    </button>
-
-                    <button class="boton-regresar" href="../Pages/menu.php" > Regresar </button>
+                <div class="div2">
+                    <p> Finaliza tu compra. </p>
+                </div>
             </div>
         </div>
 
-        <div class="div2">
-            <p> Finaliza tu compra. </p>
-        </div>
     </div>
+
+
 
 </body>
 
