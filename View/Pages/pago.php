271a7f77f9034ed281627111fe2b4b092f5b49f3
Gironmv12 <gironsan08918@gmail.com>
2023-10-31 23:53:37 -0600

    validacion de boton continuar con el pago

--- View/Pages/pago.php
+++ View/Pages/pago.php
@@ -37,7 +37,7 @@
                 </div>
                 <p id="texto-t">Tu compra estará segura, al finalizar el pago enviaremos un comprobante de compra a tu correo
                     electrónico.</p>
-
+                    <!--ARREGLAR LOS BOTONES Y USAR ANCLAS <a>-->
                     <button class="boton-minimalista" href="#" > finalizar pago 
                         <svg width="18" height="18" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" clip-rule="evenodd"
@@ -46,7 +46,7 @@
                         </svg>
                     </button>
 
-                    <button class="boton-regresar" href="../Pages/menu.php" > Regresar </button>
+                    <button class="boton-regresar" href="./menu.php" > Regresar </button>
             </div>
         </div>
 
