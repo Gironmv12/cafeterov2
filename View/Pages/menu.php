c97c54ac348d768d0d63f551bf4662404e329520
Gama2176 <gamalielmtz76@gmail.com>
2023-11-01 00:38:06 -0600

    Estilos de pago y menu restaurados con éxito

--- View/Pages/menu.php
+++ View/Pages/menu.php
@@ -332,13 +332,14 @@ if (!isset($_SESSION['carrito'])) {
                     <p class="total-compra-"> Total: </p>
                     <p id="total-compra"> $<?php echo number_format($total, 2); ?> MXN </p>
 
-                    <<a class="boton-minimalista" href="./pago.php" id="enlacePago">Continuar con el pago
+                    <div class="boton-estilos">
+                        <a class="boton-minimalista" href="./pago.php" id="enlacePago">Continuar con el pago </a>
                         <svg width="18" height="18" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" clip-rule="evenodd"
                                 d="M11 2.0625C6.06396 2.0625 2.0625 6.06396 2.0625 11C2.0625 15.936 6.06396 19.9375 11 19.9375C15.936 19.9375 19.9375 15.936 19.9375 11C19.9375 6.06396 15.936 2.0625 11 2.0625ZM14.9236 11.4861C15.0526 11.3572 15.125 11.1823 15.125 11C15.125 10.8177 15.0526 10.6428 14.9236 10.5139L12.1736 7.76386C11.9052 7.49538 11.4699 7.49538 11.2014 7.76386C10.9329 8.03235 10.9329 8.46765 11.2014 8.73614L12.7777 10.3125L7.5625 10.3125C7.1828 10.3125 6.875 10.6203 6.875 11C6.875 11.3797 7.1828 11.6875 7.5625 11.6875L12.7777 11.6875L11.2014 13.2639C10.9329 13.5323 10.9329 13.9676 11.2014 14.2361C11.4698 14.5046 11.9051 14.5046 12.1736 14.2361L14.9236 11.4861Z"
-                                fill="white"></path>
+                                fill="white" />
                         </svg>
-                        </a>
+                    </div>
                 </div>
             </div>
 
@@ -347,17 +348,20 @@ if (!isset($_SESSION['carrito'])) {
     </div>
 
     <script>
-    document.getElementById('enlacePago').addEventListener('click', function(event) {
-        if (!
-            <?php echo json_encode(isset($_SESSION['usuario_nombre']) && $_SESSION['usuario_nombre']); ?>) {
-            event.preventDefault();
-            var confirmar = confirm(
-                "Para continuar con el pago, debes iniciar sesión. ¿Deseas iniciar sesión ahora?");
-            if (confirmar) {
-                window.location.href = "./login.php";
+        document.getElementById('enlacePago').addEventListener('click', function (event) {
+            if (!
+                <
+                ?
+                php echo json_encode(isset($_SESSION['usuario_nombre']) && $_SESSION['usuario_nombre']); ? >
+            ) {
+                event.preventDefault();
+                var confirmar = confirm(
+                    "Para continuar con el pago, debes iniciar sesión. ¿Deseas iniciar sesión ahora?");
+                if (confirmar) {
+                    window.location.href = "./login.php";
+                }
             }
-        }
-    });
+        });
     </script>
 
     <!-- Script js funcionalidades -->
